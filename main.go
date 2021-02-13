84ea6290c0097797e2a277ca5ab302c2ab56f4c8
Jay Lux Ferro <jayluxferro@sperixlabs.org>
2021-02-13 13:07:30 +0000

    updated codebase

--- main.go
+++ main.go
@@ -1,12 +1,12 @@
 package main
 
 import (
+	"github.com/gin-gonic/gin"
+	_ "github.com/heroku/x/hmetrics/onload"
+	ecg "github.com/jayluxferro/ecg"
 	"log"
 	"net/http"
 	"os"
-
-	"github.com/gin-gonic/gin"
-	_ "github.com/heroku/x/hmetrics/onload"
 )
 
 func main() {
@@ -21,9 +21,33 @@ func main() {
 	router.LoadHTMLGlob("templates/*.tmpl.html")
 	router.Static("/static", "static")
 
+  var production = false
+  if(os.Getenv("MODE") == "prod"){
+    production = true
+  }
 	router.GET("/", func(c *gin.Context) {
-		c.HTML(http.StatusOK, "index.tmpl.html", nil)
+		c.HTML(http.StatusOK, "index.tmpl.html", gin.H{
+      "production": production,
+    })
 	})
 
+	// receive data
+	router.POST("/", rechargeHandler)
+
 	router.Run(":" + port)
+}
+
+func unAuthorized(c *gin.Context) {
+	c.JSON(http.StatusForbidden, gin.H{"error": "unauthorized"})
+}
+
+func rechargeHandler(c *gin.Context) {
+	meterNumber := c.PostForm("meter")
+	network := c.PostForm("network")
+	phone := c.PostForm("phone")
+	voucher := c.PostForm("voucher")
+	amount := c.PostForm("amount")
+	params := ecg.GetParams(meterNumber, phone, network, voucher, amount)
+	res := ecg.InitMakePayment(params)
+	c.String(http.StatusOK, res)
 }
